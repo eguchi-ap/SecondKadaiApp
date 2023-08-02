5666f59a34764311f6d0b23f4ed5bc7925d2fbb7
eguchi-ap <t.eguchi@alphapolis.co.jp>
2023-08-02 15:09:16 +0900

    あいさつアプリを作成する

--- SecondKadaiApp/ViewController.swift
+++ SecondKadaiApp/ViewController.swift
@@ -8,12 +8,21 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var textField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let resultViewController: ResultViewController = segue.destination as! ResultViewController
+        let name: String = textField.text!
+        resultViewController.name = name != "" ? name : "ゲスト"
+    }
+    
+    @IBAction func unwind(_ segue: UIStoryboardSegue) {
+        textField.text = nil
+    }
 }
 
